20d05a7a05c252f4a11849a9c2a4b2d50c63f985
Leonardo <89220518+leodiman182@users.noreply.github.com>
2023-03-06 11:24:41 -0300

    Mobile resume section

--- src/sections/InnerSections/ResumeSection.tsx
+++ src/sections/InnerSections/ResumeSection.tsx
@@ -33,22 +33,22 @@ const skills = [
 const ProjectsSection = () => {
   return (
     <section>
-      <h2 className="font2 text-my-pink-300 text-[34px] py-[40px] font-bold text-center px-[150px]">
+      <h2 className="font2 text-my-pink-300 text-[26px] md:text-[34px] py-[40px] font-bold text-center md:px-[150px]">
         Desenvolvedor Fullstack morando atualmente em Bauru, SP
       </h2>
       <aside className="flex flex-col items-center">
         <div className="flex flex-row w-full items-center justify-evenly">
-          <a className="bg-black text-my-pink-300 border-my-pink-300 border-[2px] py-[2px] text-[26px] px-[30px] duration-150 hover:bg-my-pink-100 hover:border-my-pink-100 hover:text-black" href="https://publuu.com/flip-book/93211/257135" download='portuguese-resume'>BAIXAR CURRÍCULO
+          <a className="bg-black text-my-pink-300 border-my-pink-300 border-[2px] py-[2px] text-[20px] md:text-[26px] px-[30px] py-[8px] duration-150 hover:bg-my-pink-100 hover:border-my-pink-100 hover:text-black" href="https://publuu.com/flip-book/93211/257135" download='portuguese-resume'>BAIXAR CURRÍCULO
           </a>
         </div>
       </aside>
-      <article className="grid grid-cols-2 my-[100px]">
-        <aside className="col-span-1 text-white ">
-          <h2 className="font2 text-my-pink-300 text-[30px] font-bold text-left px-[75px]">
+      <article className="md:grid md:grid-cols-2 my-[40px] md:my-[100px] px-[12px] md:px-0">
+        <aside className="md:col-span-1 text-white">
+          <h2 className="font2 text-my-pink-300 text-[30px] font-bold text-center md:text-left md:px-[75px] py-[18px] md:py-0">
             Bio
           </h2>
         </aside>
-        <aside className="col-span-1 pr-[75px]">
+        <aside className="md:col-span-1 md:pr-[75px] text-center md:text-left">
           <p className="font1 text-white text-[16px] font-normal">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eos optio numquam sunt rerum cupiditate. Autem molestias consequatur tempore dicta consectetur saepe commodi optio quasi maiores, odio nemo repudiandae est!
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, eos optio numquam sunt rerum cupiditate. Autem molestias consequatur tempore dicta consectetur saepe commodi optio quasi maiores, odio nemo repudiandae est!
@@ -63,23 +63,23 @@ const ProjectsSection = () => {
           </p>
         </aside>
       </article>
-      <article className="grid grid-cols-2 my-[100px]">
+      <article className="md:grid md:grid-cols-2 my-[40px] md:my-[100px] px-[12px] md:px-0">
         <aside className="col-span-1 text-white ">
-          <h2 className="font2 text-my-pink-300 text-[30px] font-bold text-left px-[75px]">
+          <h2 className="font2 text-my-pink-300 text-[30px] font-bold text-center md:text-left md:px-[75px] py-[24px] md:py-0">
             Habilidades
           </h2>
         </aside>
-        <aside className="col-span-1 grid grid-cols-3">
+        <aside className="col-span-1 md:grid md:grid-cols-3">
           {
             skills.map(el => (
-              <div className="col-span-1">
-                <h4 className="uppercase font1 text-[20px] text-my-pink-300">
+              <div className="flex flex-row md:flex-col justify-between md:justify-start row-span-1 md:col-span-1 mb-[30px] md:mb-0">
+                <h4 className="uppercase font1 md:text-[20px] text-my-pink-300">
                   { el.skillArea }
                 </h4>
-                <ul className="text-white font1 mt-[20px]">
+                <ul className="text-white font1 md:mt-[20px]">
                   {
                     el.array.map(skill => (
-                      <li className="pb-[5px] text-[18px]">{ skill }</li>
+                      <li className="pb-[5px] text-[18px] text-right md:text-left">{ skill }</li>
                     ))
                   }
                 </ul>
